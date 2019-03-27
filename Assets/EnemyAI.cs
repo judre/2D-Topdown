c994abf392198af1d0e151afbe4effae29731acf
judre <36620399+judre@users.noreply.github.com>
2019-03-27 11:15:58 +0100

    2D topdown
    **

---
+++ Assets/EnemyAI.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+    private Player player;
+   
+    public float speed;
+    private Transform playerPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+        playerPos = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, playerPos.position, speed * Time.deltaTime);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            player.health--;
+            Destroy(gameObject);
+            Debug.Log(player.health);
+        }
+        if (collision.CompareTag("Projectile"))
+        {
+            Destroy(collision.gameObject);
+            Destroy(gameObject);
+        }
+    }
+}
